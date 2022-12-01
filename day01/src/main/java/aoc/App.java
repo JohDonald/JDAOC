0783152952e9867043cf3cff83813b53d77ef4b0
personal <johannesagdonald@gmail.com>
2022-12-01 13:54:46 +0100

    Part 1 and 2 one-liners

--- day01/src/main/java/aoc/App.java
+++ day01/src/main/java/aoc/App.java
@@ -11,8 +11,11 @@ import java.util.stream.Collectors;
 public class App {
 	public static void main(String[] args) throws IOException{
 		List<Integer> test = Files.lines(Paths.get("input.txt")).reduce(new ArrayList<List<Integer>>(), (list, cals) -> {if (list.isEmpty()) {list.add(new ArrayList<>());}if (cals.length() == 0) {list.add(new ArrayList<>());} else {list.get(list.size() - 1).add(Integer.parseInt(cals));}return list;}, (list1, list2) -> {list1.addAll(list2);return list1;}).stream().reduce(new ArrayList<Integer>(), (list, calsList) -> {list.add(calsList.stream().collect(Collectors.summingInt(Integer::intValue)));return list;}).stream().sorted(Comparator.reverseOrder()).limit(3).collect(Collectors.toList());
-		System.out.println(test);
+		System.out.println("Part 1: " + test.get(0));
+		Integer test1 = test.stream().collect(Collectors.summingInt(Integer::intValue));
+		System.out.println("Part 2: " + test1);
 	}
+
 	private static void part2() throws IOException {
 		List<Integer> maxs = new ArrayList<>();
 		List<String> input = parseInput("input.txt");
