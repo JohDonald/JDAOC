ce7672c59cfd91869095c5ee0731e7a51aa7cc64
personal <johannesagdonald@gmail.com>
2022-12-06 10:46:43 +0100

    Boolean array implementation

--- day06/App.java
+++ day06/App.java
@@ -7,19 +7,14 @@ public class App {
 	public static void main(String[] args) throws IOException {
 		String input = Files.readString(Paths.get("input.txt"));
 		String part = System.getenv("part") == null ? "part1" : System.getenv("part");
-		if (part.equals("part2")) {for (int i = 0; i < input.length() - 5; i++) if(allDistinctMessage(input, i)) {System.out.println(i + 14); break;}}
-		else {for (int i = 0; i < input.length() - 5; i++) if(allDistinctPacket(input, i)) {System.out.println(i + 4); break;}}
+		if (part.equals("part2")) {for (int i = 0; i < input.length() - 5; i++) if(allDistinct(input, i, 14)) {System.out.println(i + 14); break;}}
+		else {for (int i = 0; i < input.length() - 5; i++) if(allDistinct(input, i, 4)) {System.out.println(i + 4); break;}}
 	}
-	private static boolean allDistinctPacket(String input, int i){
-		if( input.charAt(i) != input.charAt(i+1) && input.charAt(i) != input.charAt(i+2) && input.charAt(i) != input.charAt(i+3) && input.charAt(i+1) != input.charAt(i+2) && input.charAt(i+1) != input.charAt(i+3) && input.charAt(i+2) != input.charAt(i+3)) return true;
-		return false;
-	}
-	private static boolean allDistinctMessage(String input, int i){
-		int checker = 0;
-		for (int j = i; j < i + 14; j++) {
-			int val = input.charAt(j) - 'a';
-			if ((checker & (1 << val)) > 0) return false;
-			checker |= (1 << val);
+	private static boolean allDistinct(String input, int i, int pOrM){
+		boolean[] chars = new boolean['z' - 'A' + 1];
+		for (int j = i; j < i + pOrM; j++) {
+			if (!chars[input.charAt(j) - 'A']) chars[input.charAt(j) - 'A'] = true;
+			else return false;
 		}
 		return true;
 	}
